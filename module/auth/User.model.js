d8fbcdcd2824528baeb0cb691ae8edfcf96e9781
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-10-29 18:52:13 +0330

    Dev auth user model

---
+++ module/auth/User.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 2,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 2,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    },
+}, {timestamps: true});
+
+const model = mongoose.models.User || mongoose.model("User", schema);
+
+module.exports = model;
