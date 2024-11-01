9571838b7c5f88a2493d6e2973719cfb6669876d
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-11-01 13:39:17 +0330

    Development ban user controller, get all banned users

--- modules/banUser/banUser.controller.js
+++ modules/banUser/banUser.controller.js
@@ -1,4 +1,18 @@
+const banUserModel = require('./BanUser.model');
+
 exports.getAll = async (req, res) => {
+    try {
+        const bannedUsers = await banUserModel.find({})
+            .populate("user", "-password -__v").lean();
+
+        res.json({...bannedUsers});
+
+    } catch (err) {
+        console.log(`ban user controller, get all error: ${err}`);
+        return res.status(500).json({
+            message: err.message,
+        });
+    }
 };
 
 exports.banUser = async (req, res) => {
