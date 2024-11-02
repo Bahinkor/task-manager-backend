7c223f27eb1c81f65fe23695e4ea0178c19f15f9
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-11-02 21:52:58 +0330

    Development task model

---
+++ modules/task/Task.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    undertaking: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    manager: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["waiting", "accepted", "rejected", "canceled", "done"],
+        required: true,
+    },
+    text: {
+        type: String,
+        minLength: 3,
+        maxLength: 9000,
+        required: true,
+    },
+    Attach: {
+        type: String,
+        required: true,
+    },
+}, {timestamps: true});
+
+const model = mongoose.models.Task || mongoose.model("Task", schema);
+
+module.exports = model;
